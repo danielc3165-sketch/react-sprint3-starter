fdae22ccf880acf4415d0076b33f51fa27a37a7a
danielc3165-sketch <daniel.c3165@gmail.com>
2026-08-11 15:43:35 +0300

    +MainList

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -1,3 +1,24 @@
-export function MailList() {
-    return <div>Mail list</div>
+const { useState } = React
+
+export function MailList({mails}) {
+    
+  const [ mailsToEdit,setMailsToEdit ] = useState(mails)
+  console.log('mailsTE',mailsToEdit)
+
+  return <div>
+
+    <ul className="mail-list-ul">
+      {mailsToEdit.map(mail=><li key={mail.id}>
+
+       <p>{mail.subject}</p>
+       <p>{mail.body}</p>
+
+
+      </li>)}
+
+
+
+    </ul>
+
+  </div>
 }
--- apps/mail/pages/MailIndex.jsx
+++ apps/mail/pages/MailIndex.jsx
@@ -1,5 +1,32 @@
 
-export function MailIndex() {
-    return <section className="container">Mail app</section>
+import { utilService } from "../../../services/util.service.js"
+import { mailService } from "../services/mail.service.js"
+
+import { MailList } from "../cmps/MailList.jsx"
+
+const { useState,useEffect } = React
+
+
+export function MailIndex({mailsData}) {
+
+    const [ mails,setMails] = useState(mailsData)
+    
+
+    //  useEffect(()=>{
+    //     mailService.query()
+    //     .then(data=>{
+    //         setMails(data)
+    //         console.log('data',data)
+    //     })
+    //  },[])
+
+     if(!mails) return
+
+    return <section className="container">
+        <h1>Mail app</h1>
+
+        <MailList mails={mails}/>
+        
+        </section>
 }
 
